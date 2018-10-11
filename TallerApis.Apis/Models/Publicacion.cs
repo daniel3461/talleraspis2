341ce4347bd5f4bb57fb155179e60c7c3a2decd6
503 <503@academicos.cesde.edu.co>
2018-10-10 20:17:31 -0500

    Agregar archivos de proyecto.

---
+++ TallerApis.Apis/Models/Publicacion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TallerApis.Apis.Models
+{
+    public class Publicacion : DbContext
+    {
+       public Publicacion() : base("PublicacionConnection")
+        {
+
+        }
+        public DbSet<tblpublicacion> Publicaciones { get; set; }
+    }
+}
